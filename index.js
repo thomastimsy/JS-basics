9b8189aa7b10d4f427b71b5f0302e5e3facf0318
thomastimsy <thomastimsytom@gmail.com>
2024-09-22 17:30:22 +0100

    added some js code

---
+++ index.js
@@ -0,0 +1,29 @@
+let thingOne = "red";
+let thingTwo = "blue";
+console.log("red" + " blue");
+console.log(thingOne + " " + thingTwo);
+console.log(1/0);
+console.log(`The colour is ${thingOne}`);
+let isGreater = 4 > 1;
+console.log(isGreater)
+/*let age = prompt("How old are you?", 30);
+console.log(`I am about ${age} years old`);*/
+let bill = 10.25 + 3.99 + 7.15;
+let tip = 0.15 * bill;
+console.log(bill + tip);
+let total = 24.5985;
+console.log(total.toFixed(2));
+//The Intro to JavaScript course is amazing. James and Julia are so fun. I cannot wait to work through the rest of this entertaining content!
+let adjective1 = "amazing";
+let adjective2 = "fun";
+let adjective3 = "entertaining";
+let madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`;
+console.log(madLib);
+
+
+
+
+
+
+
+
