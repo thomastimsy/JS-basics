780999fb15ba12767ad26142039cb6ce5290c4eb
thomastimsy <thomastimsytom@gmail.com>
2024-10-10 16:05:46 +0100

    added new commit

--- index.js
+++ index.js
@@ -19,8 +19,63 @@ let adjective2 = "fun";
 let adjective3 = "entertaining";
 let madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`;
 console.log(madLib);
+let firstName = "James";
+let interest = "baseBall";
+let hobby = "read";
+let awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`;
+console.log(awesomeMessage);
+console.log(32 % 9);
+console.log(5 ** 5);
+let apples = "2";
+let oranges = "3";
+console.log(apples + oranges);
+ apples = "2";
+ oranges = "3";
+console.log( +apples + +oranges );
+let counter = 2;
+counter++;
+console.log(counter);
+/*let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';
+  console.log(message);
+using else if..same as
+  if (age < 3) {
+  message = 'Hi, baby!';
+} else if (age < 18) {
+  message = 'Hello!';
+} else if (age < 100) {
+  message = 'Greetings!';
+} else {
+  message = 'What an unusual age!';
+} */
+
+/*let answer;
+let officialName = prompt("what is the official name of Javascript", "");  
+if (officialName == ECMASCRIPT); {
+    answer = "RIGHT!"; 
+    }
+else {
+      answer = "You don’t know? ECMAScript!";
+    }*/ 
+    //didnt work on console
+
+    
+    let value = prompt('What is the "official" name of JavaScript?', '');
+
+    if (value == 'ECMAScript') {
+      alert('Right!');
+    } else {
+      alert("You don't know? ECMAScript!");
+    } //this worked
+
+    
 
 
+     
 
 
 
---
+++ practice.js
@@ -0,0 +1,15 @@
+let hour = 9;
+
+if (hour < 10 || hour > 18) {
+  console.log( 'The office is closed.' );
+}
+let hourr = 12;
+let minute = 30;
+
+if (hourr == 12 && minute == 30) {
+  console.log( 'The time is 12:30' );
+}
+if (1 && 0) { // evaluated as true && false
+    alert( "won't work, because the result is falsy" );
+  } 
+
